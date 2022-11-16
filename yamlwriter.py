3f0e78abd06e100e51bb66824d9670d930ba3876
Robert Muchsel <robert.muchsel@maximintegrated.com>
2022-11-16 09:23:41 -0600

    Merge branch feature/pytorch-1.12

--- yamlwriter.py
+++ yamlwriter.py
@@ -371,6 +371,8 @@ def create(
             else:
                 this_layer['avg_pool'] = shape
             this_layer['pool_stride'] = all_ops[name]['attrs']['strides'][0]
+            if 'dilations' in all_ops[name]['attrs']:
+                this_layer['pool_dilation'] = all_ops[name]['attrs']['dilations']
         elif op in ('Abs', 'Relu'):
             this_layer['activate'] = op
         else:
@@ -588,6 +590,8 @@ def create(
                 ll['max_pool'] = prev['max_pool']
             if 'pool_stride' in prev:
                 ll['pool_stride'] = prev['pool_stride']
+            if 'pool_dilation' in prev:
+                ll['pool_dilation'] = prev['pool_dilation']
             if 'data_format' in prev:
                 ll['data_format'] = prev['data_format']
 
@@ -655,6 +659,8 @@ def create(
                 ll['pool_first'] = 'false'
             if 'pool_stride' in prev:
                 ll['pool_stride'] = prev['pool_stride']
+            if 'pool_dilation' in prev:
+                ll['pool_dilation'] = prev['pool_dilation']
             if 'data_format' in prev:
                 ll['data_format'] = prev['data_format']
             ll['eltwise'] = prev['eltwise']
@@ -1200,6 +1206,8 @@ def create(
                 f.write(f"    avg_pool: {ll['avg_pool']}\n")
             if 'pool_stride' in ll:
                 f.write(f"    pool_stride: {ll['pool_stride']}\n")
+            if 'pool_dilation' in ll:
+                f.write(f"    pool_dilation: {ll['pool_dilation']}\n")
             if 'write_gap' in ll:
                 f.write(f"    write_gap: {ll['write_gap']}\n")
             if 'output_processors' in ll:
